a21769be0ca97b038873b0896120512084f97476
infeeeee <gyetpet@mailbox.org>
2024-08-30 02:21:28 +0200

    Support more commands, reorder, raise error with screenshot and camshot (#25)
    * Support more commands, reorder, raise error with screenshot and camshot
    
    * Fix Daydream commands, comments

--- fullykiosk/__init__.py
+++ fullykiosk/__init__.py
@@ -25,9 +25,16 @@ class FullyKiosk:
         data = await self._rh.get(
             cmd=cmd, password=self._password, type="json", **kwargs
         )
-        if RESPONSE_STATUS in data and data[RESPONSE_STATUS] == RESPONSE_ERRORSTATUS:
+
+        if (
+            isinstance(data, dict)
+            and RESPONSE_STATUS in data
+            and data[RESPONSE_STATUS] == RESPONSE_ERRORSTATUS
+        ):
             raise FullyKioskError(RESPONSE_ERRORSTATUS, data[RESPONSE_STATUSTEXT])
         return data
+
+    # REST API Documentation: https://www.fully-kiosk.com/en/#rest
 
     async def getDeviceInfo(self):
         result = await self.sendCommand("deviceInfo")
@@ -47,49 +54,7 @@ class FullyKiosk:
     def settings(self):
         return self._settings
 
-    async def startScreensaver(self):
-        await self.sendCommand("startScreensaver")
-
-    async def stopScreensaver(self):
-        await self.sendCommand("stopScreensaver")
-
-    async def screenOn(self):
-        await self.sendCommand("screenOn")
-
-    async def screenOff(self):
-        await self.sendCommand("screenOff")
-
-    async def setScreenBrightness(self, brightness):
-        await self.sendCommand(
-            "setStringSetting", key="screenBrightness", value=brightness
-        )
-
-    async def setAudioVolume(self, volume, stream=None):
-        await self.sendCommand("setAudioVolume", level=volume, stream=stream)
-
-    async def restartApp(self):
-        await self.sendCommand("restartApp")
-
-    async def loadStartUrl(self):
-        await self.sendCommand("loadStartUrl")
-
-    async def loadUrl(self, url):
-        await self.sendCommand("loadUrl", url=url)
-
-    async def playSound(self, url, stream=None):
-        await self.sendCommand("playSound", url=url, stream=stream)
-
-    async def stopSound(self):
-        await self.sendCommand("stopSound")
-
-    async def toForeground(self):
-        await self.sendCommand("toForeground")
-    
-    async def toBackground(self):
-        await self.sendCommand("toBackground")
-
-    async def startApplication(self, application):
-        await self.sendCommand("startApplication", package=application)
+    # Configurations
 
     async def setConfigurationString(self, setting, stringValue):
         await self.sendCommand("setStringSetting", key=setting, value=stringValue)
@@ -97,11 +62,54 @@ class FullyKiosk:
     async def setConfigurationBool(self, setting, boolValue):
         await self.sendCommand("setBooleanSetting", key=setting, value=boolValue)
 
-    async def enableLockedMode(self):
-        await self.sendCommand("enableLockedMode")
+    # Screen, screensaver
 
-    async def disableLockedMode(self):
-        await self.sendCommand("disableLockedMode")
+    async def screenOn(self):
+        await self.sendCommand("screenOn")
+
+    async def screenOff(self):
+        await self.sendCommand("screenOff")
+
+    async def forceSleep(self):
+        await self.sendCommand("forceSleep")
+
+    async def startScreensaver(self):
+        await self.sendCommand("startScreensaver")
+
+    async def stopScreensaver(self):
+        await self.sendCommand("stopScreensaver")
+
+    # Daydream: max Android 12
+
+    async def startDaydream(self):
+        await self.sendCommand("startDaydream")
+
+    async def stopDaydream(self):
+        await self.sendCommand("stopDaydream")
+
+    async def setScreenBrightness(self, brightness):
+        await self.setConfigurationString("screenBrightness", brightness)
+
+    # Audio
+
+    async def setAudioVolume(self, volume, stream=None):
+        await self.sendCommand("setAudioVolume", level=volume, stream=stream)
+
+    async def playSound(self, url, stream=None):
+        await self.sendCommand("playSound", url=url, stream=stream)
+
+    async def stopSound(self):
+        await self.sendCommand("stopSound")
+
+    async def textToSpeech(self, text, locale=None, engine=None, queue=None):
+        if queue is not None:
+            queue = "1" if queue else "0"
+        await self.sendCommand("textToSpeech", text=text, locale=locale, engine=engine, queue=queue)
+
+    async def stopTextToSpeech(self):
+        await self.sendCommand("stopTextToSpeech")
+
+    # Lock, maintenance
 
     async def lockKiosk(self):
         await self.sendCommand("lockKiosk")
@@ -109,29 +117,75 @@ class FullyKiosk:
     async def unlockKiosk(self):
         await self.sendCommand("unlockKiosk")
 
+    async def enableLockedMode(self):
+        await self.sendCommand("enableLockedMode")
+
+    async def disableLockedMode(self):
+        await self.sendCommand("disableLockedMode")
+
+    # Root only:
+
+    async def rebootDevice(self):
+        await self.sendCommand("rebootDevice")
+
+    # App management
+
+    async def restartApp(self):
+        await self.sendCommand("restartApp")
+
+    async def exitApp(self):
+        await self.sendCommand("exitApp")
+
+    async def killMyProcess(self):
+        await self.sendCommand("killMyProcess")
+
+    async def toForeground(self):
+        await self.sendCommand("toForeground")
+
+    async def toBackground(self):
+        await self.sendCommand("toBackground")
+
+    async def startApplication(self, application):
+        await self.sendCommand("startApplication", package=application)
+
+    # Web browsing
+
+    async def loadStartUrl(self):
+        await self.sendCommand("loadStartUrl")
+
+    async def loadUrl(self, url):
+        await self.sendCommand("loadUrl", url=url)
+
+    async def clearCache(self):
+        await self.sendCommand("clearCache")
+
+    async def clearWebstorage(self):
+        await self.sendCommand("clearWebstorage")
+
+    async def clearCookies(self):
+        await self.sendCommand("clearCookies")
+
+    async def resetWebview(self):
+        await self.sendCommand("resetWebview")
+
+    # Motion detection
+
+    async def triggerMotion(self):
+        await self.sendCommand("triggerMotion")
+
     async def enableMotionDetection(self):
         await self.setConfigurationBool("motionDetection", True)
 
     async def disableMotionDetection(self):
         await self.setConfigurationBool("motionDetection", False)
 
-    async def rebootDevice(self):
-        await self.sendCommand("rebootDevice")
-
-    async def clearCache(self):
-        await self.sendCommand("clearCache")
-    
-    async def clearWebstorage(self):
-        await self.sendCommand("clearWebstorage")
-    
-    async def clearCookies(self):
-        await self.sendCommand("clearCookies")
+    # Camera, screenshot:
 
     async def getCamshot(self):
-        return await self._rh.get(cmd="getCamshot", password=self._password)
+        return await self.sendCommand("getCamshot")
 
     async def getScreenshot(self):
-        return await self._rh.get(cmd="getScreenshot", password=self._password)
+        return await self.sendCommand("getScreenshot")
 
 
 class _RequestsHandler:
